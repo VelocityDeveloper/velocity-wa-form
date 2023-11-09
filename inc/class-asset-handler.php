57309d4b3d89f71d738431d0431a4372667fd8c9
aadiityaak <aadiityaak@gmail.com>
2023-11-09 13:47:29 +0700

    add asset handler

---
+++ inc/class-asset-handler.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ *
+ * @link       https://velocitydeveloper.com
+ * @since      1.0.0
+ *
+ * @package    Velocity_wa_form
+ * @subpackage Velocity_wa_form/inc
+ */
+
+class Asset_Handler {
+    public function __construct() {
+        add_action('wp_enqueue_scripts', array($this, 'enqueue_assets'));
+    }
+
+    public function enqueue_assets() {
+        wp_enqueue_style('custom-style', plugin_dir_url(__FILE__) . '../assets/style.css');
+        wp_enqueue_script('custom-script', plugin_dir_url(__FILE__) . '../assets/script.js', array('jquery'), '1.0', true);
+    }
+}
---
+++ velocity-wa-form.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ *
+ * @link              https://velocitydeveloper.com
+ * @since             1.0.0
+ * @package           Velocity_wa_form
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Velocity Whatsapp Form
+ * Plugin URI:        https://velocitydeveloper.com
+ * Description:       Addon plugin for Velocitydeveloper Client
+ * Version:           1.0.0
+ * Author:            Velocity
+ * Author URI:        https://velocitydeveloper.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       velocity-wa-form
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if (!defined('WPINC')) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define('VELOCITY_WA_FORM_VERSION', '1.0.1');
